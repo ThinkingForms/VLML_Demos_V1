fe69c9b55ffde4f192173e806dbc7bbc978a1c53
YanYas <hayden.anyasi@network.rca.ac.uk>
2019-03-15 22:45:23 +0000

    Demos and Utilities. A set of demos and some utilities to make it easy to download datasets and models. Utilities include means to extract data from gz and tgz files. Need to work out what to do about the nuget  dependencies.

---
+++ Utilities/src/DemoUtils/Downloading/ModelEnums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using VL.Lib.Collections;
+
+namespace ModelEnums
+{
+    public class ModelPickDefinition : ManualDynamicEnumDefinitionBase<ModelPickDefinition>
+    {
+        public static ModelPickDefinition Instance => ManualDynamicEnumDefinitionBase<ModelPickDefinition>.Instance;
+
+        override protected bool AutoSortAlphabetically => false;
+    }
+
+    [Serializable]
+    public class ModelPick : DynamicEnumBase<ModelPick, ModelPickDefinition>
+    {
+        public ModelPick(string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static ModelPick CreateDefault() => CreateDefaultBase("No Model Found");
+        //{
+        //    //use method of base class if nothing special required
+        //    return ModelPick CreateDefaultBase() 
+        //}
+    }
+}
